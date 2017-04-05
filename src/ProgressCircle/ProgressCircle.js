384ee29ffdf8a090b014757d071537f2f7bef09e
Masatoshi Nishiguchi <nishiguchi.masa@gmail.com>
2017-04-04 20:03:54 -0400

    Process the stroke dash animation asynchnously

--- src/ProgressCircle/ProgressCircle.js
+++ src/ProgressCircle/ProgressCircle.js
@@ -3,42 +3,56 @@ import classNames from 'classnames'
 
 import './ProgressCircle.css'
 
-const ProgressCircle = (props) => {
-  const { label, score, children } = props
+class ProgressCircle extends React.Component {
+  render() {
+    const { label, score, children } = this.props
 
-  const progressClassName = classNames({
-    'rating-progress': true,
-    'under-50'       : (score <= 50),
-    'under-75'       : (score <= 75 && score > 50),
-    'over-75'        : (score > 75),
-  })
+    const progressClassName = classNames({
+      'rating-progress': true,
+      'under-50'       : (score <= 50),
+      'under-75'       : (score <= 75 && score > 50),
+      'over-75'        : (score > 75),
+    })
 
-  const progressStyle = {
-    strokeDashoffset: 100 - score
+    return (
+      <div className="ProgressCircle">
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 34 34">
+          <circle
+            cx="16" cy="16" r="15.9155"
+            className="rating-progress-background"
+          />
+          <circle
+            cx="16" cy="16" r="15.9155"
+            className={progressClassName}
+            ref={node => this.circleNode = node}
+          />
+        </svg>
+        {children}
+        <div className="labelText">
+          <a href="#">{label || ""}</a>
+        </div>
+        <div className="scoreText">
+          {Number(score) || 0}
+        </div>
+      </div>
+    )
   }
 
-  return (
-    <div className="ProgressCircle">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="-1 -1 34 34">
-        <circle
-          cx="16" cy="16" r="15.9155"
-          className="rating-progress-background"
-        />
-        <circle
-          cx="16" cy="16" r="15.9155"
-          className={progressClassName}
-          style={progressStyle}
-        />
-      </svg>
-      {children}
-      <div className="labelText">
-        <a href="#">{label || ""}</a>
-      </div>
-      <div className="scoreText">
-        {Number(score) || 0}
-      </div>
-    </div>
-  )
+  componentDidMount() {
+    this.triggerStrokeDash()
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    this.triggerStrokeDash()
+  }
+
+  // Process the stroke dash animation asynchnously.
+  triggerStrokeDash(score) {
+    setTimeout(() => {
+      this.circleNode.style.strokeDashoffset = 100
+      this.circleNode.style.strokeDashoffset = 100 - this.props.score
+    }, 0)
+  }
 }
 
 export default ProgressCircle
